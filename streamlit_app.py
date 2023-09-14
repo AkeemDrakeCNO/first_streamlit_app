9fbf724c0c90025357c26354a1460fa497e5e7fe
AkeemDrakeCNO <144727138+AkeemDrakeCNO@users.noreply.github.com>
2023-09-14 15:05:59 -0400

    Update streamlit_app.py
    removed watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 #new Section to dsplay fruityvice api respose
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
 
